4626b93f9ec254f3d2520bb103bdfc3d7450652e
adoleba <ania.doleba@wp.pl>
2019-05-13 11:12:46 +0200

    Book, Author, Category models, Admin register

--- books_app/books/models.py
+++ books_app/books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    author = models.ManyToManyField(Author)
+    category = models.ManyToManyField(Category)
