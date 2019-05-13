e99bbe7cb9b04035d2bcb1df18f470ab74960981
adoleba <ania.doleba@wp.pl>
2019-05-13 12:20:23 +0200

    books_list template and view

--- books_app/books/views.py
+++ books_app/books/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from books_app.books.models import Book
+
 
 def index(request):
-    return HttpResponse('Index')
+    books = Book.objects.all()
+    return render(request, 'books/templates/books_index.html', {'books': books})
 
 
 def add_book(request):
