c35054954a5eac9eacdbe043cd35fecfb56fd7a5
adoleba <ania.doleba@wp.pl>
2019-05-13 11:17:56 +0200

    __str__ to all models

--- books_app/books/models.py
+++ books_app/books/models.py
@@ -4,9 +4,15 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -14,3 +20,7 @@ class Book(models.Model):
     description = models.TextField()
     author = models.ManyToManyField(Author)
     category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
