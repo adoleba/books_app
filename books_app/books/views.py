f45a81b6ccbefbb5b6e2cd932b83fbf6e273d095
adoleba <ania.doleba@wp.pl>
2019-05-14 17:07:46 +0200

    fixed function set_import

--- books_app/books/views.py
+++ books_app/books/views.py
@@ -38,33 +38,16 @@ def add_book(request):
 
 def add_author(request):
     if request.method == "POST":
-        form = ImportForm(request.POST)
+        form = AuthorForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             author = Author.objects.create(name=name)
         return redirect('add_book')
 
     else:
-        form = Book(request.POST)
+        form = AuthorForm(request.POST)
 
     return render(request, 'books/templates/add_author.html', {'form': form})
-
-
-def import_book(request, value_url):
-    response = requests.get(value_url)
-    data = response.json()
-    books_data = data['items']
-    for value in books_data:
-        book_dict = value['volumeInfo']
-        title = book_dict['title']
-        description = book_dict['publisher']
-        authors = book_dict['authors']
-        categories = book_dict['authors']
-        book = Book.objects.create(title=title, description=description)
-        for author in authors:
-            book.author.create(name=author)
-        for category in categories:
-            book.category.create(name=category)
 
 
 def set_import(request):
@@ -83,5 +66,39 @@ def set_import(request):
     return render(request, 'books/templates/import_book.html', {'form': form})
 
 
+def import_book(request, value_url):
+    response = requests.get(value_url)
+    data = response.json()
+    books_data = data['items']
+    for value in books_data:
+        book_dict = value['volumeInfo']
+        title = book_dict['title']
 
+        if 'description' in book_dict:
+            description = book_dict['description']
+        else:
+            description = "No description"
+        authors = book_dict['authors']
 
+        if 'categories' in book_dict:
+            categories = book_dict['categories']
+        else:
+            categories = ""
+
+        book = Book.objects.create(title=title, description=description)
+        for author in authors:
+            authors_in_qs = Author.objects.values()
+            list_of_authors = [author['name'] for author in authors_in_qs]
+            if author in list_of_authors:
+                author_name = Author.objects.get(name=author)
+                book.author.add(author_name)
+            else:
+                book.author.create(name=author)
+        for category in categories:
+            categories_in_qs = Category.objects.values()
+            list_of_categories = [author['name'] for author in categories_in_qs]
+            if category in list_of_categories:
+                category_name = Category.objects.get(name=category)
+                book.category.add(category_name)
+            else:
+                book.category.create(name=category)
